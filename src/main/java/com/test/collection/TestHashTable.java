1196fde35c9a193679134c4618c7cb76499082b0
yangjianzhou <yangjianzhouctgu@126.com>
2016-03-26 21:01:44 +0800

    init jerseyTest

---
+++ src/main/java/com/test/collection/TestHashTable.java
@@ -0,0 +1,17 @@
+package com.test.collection;
+
+import java.util.Hashtable;
+
+/**
+ * Created by yangjianzhou on 16-3-6.
+ */
+public class TestHashTable {
+
+    public static void main(String[] args) {
+
+/*        Hashtable<String , String>  hashtable = new Hashtable<>();
+        System.out.println(hashtable.get("1"));*/
+
+        System.out.println(5&6);
+    }
+}
